c8a16a3a787f0d008b270fa45bc75910a1f97469
shile <1019606614@qq.com>
2015-07-24 15:55:02 +0800

    Initial Commit

---
+++ SkipWebToClient/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SkipWebToClient
+//
+//  Created by Kevin on 15/7/24.
+//  Copyright (c) 2015年 石乐. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
